1b2712730628d4be17a3c9bd7767dec927063c5d
haddouhemilissa <151778477+HaddoucheMilissa@users.noreply.github.com>
2023-12-27 19:56:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-from statistics import mean
+#model for breast cancer
 import numpy as np
-import random
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use('ggplot')
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
 
-#DATA CREATION:
-def create_data(hm,variance,step=2,correlation=False):
-  val=1
-  ys=[]
-  for i in range(hm):
-    y=val+random.randrange(-variance, variance)
-    ys.append(y)
-    if correlation and correlation=='pos':
-      val+=step
-    elif correlation and correlation=='neg':
-      val-=step
-    xs =[i for i in range(len(ys))]
-  return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
-xs,ys=create_data(40,20,2,'neg')  # correlation='neg' so we see in the curve that the data is descending
-plt.scatter(xs,ys,label='data')
-def best_fit(xs,ys):
-  m=(((mean(xs)*mean(ys))-mean(xs*ys)))/((mean(xs)**2)-mean(xs*xs))
-  b= mean(ys)-m*mean(xs)
-  return m,b
-m,b=best_fit(xs,ys)
-#calculat the regression line
-regression_line=[(m*x)+b for x in xs]
-plt.plot(xs,regression_line,label='regression line')
+ds= datasets.load_breast_cancer()
+x,y = ds.data, ds.target 
+print(ds.target_names)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+
+from sklearn.linear_model import LogisticRegression 
+lr= LogisticRegression()
+lr.fit(x_train,y_train)
+prediction= lr.predict(x_test)
+
+def accuracy(y_true, y_prediction):
+  y_true== y_prediction 
+  return np.sum(y_true== y_prediction)/len(y_true)
+print('accuracy class:',accuracy(y_test,prediction))
